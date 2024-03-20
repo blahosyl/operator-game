288571d47a35805f7bfc36e493e789216f52bc80
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-03-20 21:08:36 +0100

    break up function for generating operators | needs work
    - the loop generating the `randomOperatorNumbers` is rerun 3 more times when `calculateSolution` is called

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 function newGame() {
-  console.log('Get operands',getOperands());
   console.log('showRandomOperands',showRandomOperands());
-  // calculateSolution();
+
+  console.log('chosenOperators:',generateRandomOperators());
+
+  calculateSolution();
+
 }
 
 // run this function when refreshing the page
@@ -57,44 +60,25 @@ function showRandomOperands() {
   for (let i = 0; i < operandsComputed.length; i++) {
     operands[i].innerHTML = operandsComputed[i];
   }
-
-  // for (let operand in operands) {
-  //   operand.innerHTML = operandsComputed[operand];
-  // }
-
-  console.log('type of operands:',typeof operands);
-  console.log('operands: element', operands);
-  console.log('operands keys:', Object.keys(operands));
-  console.log('operands length:', operands.length);
   return operandsComputed;
 }
 
-// THIS FUNCTION SHOULD BE BROKEN UP INTO SMALLER PIECES
-//replace all operands with random integers between 1 and 10 (incl)
-function randomOperands() {
-  // get the array of operands
-  let operands = document.getElementsByClassName('operand');
-  // empty array of values for calculation
-  let operandValues = [];
-
-  // replace each operand with a different random integer
-  for (let operand of operands) {
-    // generate random integer between 1 and 10
-    let randomNumber = Math.floor(Math.random()*11);
-    // make sure number is not 0
-    while (randomNumber === 0) {
-      randomNumber = Math.floor(Math.random()*11);
-    }
-    // show the random integer in the HTML
-    operand.innerHTML = randomNumber;
-    console.log(operand.innerHTML);
-    // add the random integer to the array of values for calculation
-    operandValues.push(operand.innerHTML*1);
+function generateRandomOperators() {
+  let randomOperatorNumbers = generateRandomNumbers(4);
+  console.log('randomOperatorNumbers: ',randomOperatorNumbers);
+  // the operators the program can chose from
+  let operators = ["+","-","*","/"];
+  // the array that will be filled with the randomly chosen operators at each game
+  let chosenOperators = [];
+  for (let i = 0; i<3; i++) {
+    // add an element from `operators`based on the random number
+    chosenOperators.push(operators[randomOperatorNumbers[i]]);
   }
-  console.log(operandValues);
-  return operandValues;
+  return chosenOperators;
 }
 
+
+// TO BE BROKEN UP
 // pick random operators: +, -, * or /
 function randomOperators() {
   // the operators the program can chose from
@@ -115,24 +99,24 @@ function randomOperators() {
   return chosenOperators;
 }
 
-// TRY TO BREAK UP FUNCTIONS INTO SMALLER PIECES:
-// 1. RANDOM NUMBER GENERATOR FOR INTEGERS, ADDS VALUES TO ARRAY `operandValues`
-// 2. function to change `innerHTML` using `operandValues`
 
-
+// calculate the correct solution based on the random numbers and random operators
 function calculateSolution() {
+  // define empty string for solution
   let solution = '';
+  // alternately add a number and an operator
   for (let i = 0; i<3; i++) {
-    solution+=randomOperands()[i];
-    solution+=randomOperators()[i];
+    solution+=getOperands()[i];
+    solution+=generateRandomOperators()[i];
   }
 
   // cut off the last operand from the end of the string
   solution = solution.slice(0, -1);
 
-  solutionValue = eval(solution);
 
-  console.log(solution+'='+solutionValue);
+  let solutionValue = eval(solution);
+
+  console.log('the correct solution:',solution+'='+solutionValue);
 
   // shows the correct solution for now. to be changed to show calculation of incorrect solution only
   let solutionDiv = document.getElementById('solution');
