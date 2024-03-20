2bc7c347c13615a61fd57317db570660c0c82b82
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-03-20 01:11:37 +0100

    put random operand code in a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
-// get the array of operands
-let operands = document.getElementsByClassName('operand');
+//replace all operands with random integers between 0 and 10 (incl)
+function randomOperands() {
+  // get the array of operands
+  let operands = document.getElementsByClassName('operand');
 
-// replace each operand with a different random number
-for (let operand of operands) {
-  // generate random number between 0 and 10
-  let randomNumber = Math.floor(Math.random()*11);
-  operand.innerHTML = randomNumber;
+  // replace each operand with a different random integer
+  for (let operand of operands) {
+    // generate random integer between 0 and 10
+    let randomNumber = Math.floor(Math.random()*11);
+    operand.innerHTML = randomNumber;
+  }
 }
+
+randomOperands()
